89ddd7a788bf3e1f570463c1e629c599b10371c7
syunkitada <syun.kitada@gmail.com>
2021-06-27 19:37:09 +0900

    update

--- kubernetes/1.21.0/20_setup_etcd.sh
+++ kubernetes/1.21.0/20_setup_etcd.sh
@@ -54,7 +54,7 @@ sudo systemctl daemon-reload
 sudo systemctl enable etcd
 sudo systemctl start etcd
 
-sudo ETCDCTL_API=3 etcdctl member list \
+sudo ETCDCTL_API=3 /usr/local/bin/etcdctl member list \
   --endpoints=https://127.0.0.1:2379 \
   --cacert=/etc/etcd/ca.pem \
   --cert=/etc/etcd/kubernetes.pem \
