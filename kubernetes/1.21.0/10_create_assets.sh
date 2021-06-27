e26f0ae186f8267f8ab8404e562c2ee58ae92c77
syunkitada <syun.kitada@gmail.com>
2021-06-27 18:44:09 +0900

    update

--- kubernetes/1.21.0/10_create_assets.sh
+++ kubernetes/1.21.0/10_create_assets.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -xe
 
 if [ $# != 1 ]; then
-    echo "help: input node_ip"
+    echo "ERROR: Input node_ip"
     echo "./10_setup_cfgs.sh [node_ip]"
     exit 1
 fi
 
 NODE_IP=$1
 API_IP=${NODE_IP}
-mkdir ~/k8s-pems
-cd ~/k8s-pems
+mkdir ~/k8s-assets
+cd ~/k8s-assets
 
 # Create CA(Certificate Authority)
 # ca-key.pem, ca.pem
@@ -272,3 +272,98 @@ cfssl gencert \
   -profile=kubernetes \
   service-account-csr.json | cfssljson -bare service-account
 fi
+
+# ---------------------------------------------
+# Create kubernetes configs
+# ---------------------------------------------
+# The kubelet Kubernetes Configuration File
+kubectl config set-cluster kubernetes-the-hard-way \
+  --certificate-authority=ca.pem \
+  --embed-certs=true \
+  --server=https://${API_IP}:6443 \
+  --kubeconfig=${NODE_IP}.kubeconfig
+kubectl config use-context default --kubeconfig=${NODE_IP}.kubeconfig
+
+
+# The kube-proxy Kubernetes Configuration File
+kubectl config set-cluster kubernetes-the-hard-way \
+    --certificate-authority=ca.pem \
+    --embed-certs=true \
+    --server=https://${API_IP}:6443 \
+    --kubeconfig=kube-proxy.kubeconfig
+
+kubectl config set-credentials system:kube-proxy \
+    --client-certificate=kube-proxy.pem \
+    --client-key=kube-proxy-key.pem \
+    --embed-certs=true \
+    --kubeconfig=kube-proxy.kubeconfig
+
+kubectl config set-context default \
+    --cluster=kubernetes-the-hard-way \
+    --user=system:kube-proxy \
+    --kubeconfig=kube-proxy.kubeconfig
+
+kubectl config use-context default --kubeconfig=kube-proxy.kubeconfig
+
+
+# The kube-controller-manager Kubernetes Configuration File
+kubectl config set-cluster kubernetes-the-hard-way \
+    --certificate-authority=ca.pem \
+    --embed-certs=true \
+    --server=https://127.0.0.1:6443 \
+    --kubeconfig=kube-controller-manager.kubeconfig
+
+kubectl config set-credentials system:kube-controller-manager \
+    --client-certificate=kube-controller-manager.pem \
+    --client-key=kube-controller-manager-key.pem \
+    --embed-certs=true \
+    --kubeconfig=kube-controller-manager.kubeconfig
+
+kubectl config set-context default \
+    --cluster=kubernetes-the-hard-way \
+    --user=system:kube-controller-manager \
+    --kubeconfig=kube-controller-manager.kubeconfig
+
+kubectl config use-context default --kubeconfig=kube-controller-manager.kubeconfig
+
+
+# The kube-scheduler Kubernetes Configuration File
+kubectl config set-cluster kubernetes-the-hard-way \
+    --certificate-authority=ca.pem \
+    --embed-certs=true \
+    --server=https://127.0.0.1:6443 \
+    --kubeconfig=kube-scheduler.kubeconfig
+
+kubectl config set-credentials system:kube-scheduler \
+    --client-certificate=kube-scheduler.pem \
+    --client-key=kube-scheduler-key.pem \
+    --embed-certs=true \
+    --kubeconfig=kube-scheduler.kubeconfig
+
+kubectl config set-context default \
+    --cluster=kubernetes-the-hard-way \
+    --user=system:kube-scheduler \
+    --kubeconfig=kube-scheduler.kubeconfig
+
+kubectl config use-context default --kubeconfig=kube-scheduler.kubeconfig
+
+
+# The admin Kubernetes Configuration File
+kubectl config set-cluster kubernetes-the-hard-way \
+  --certificate-authority=ca.pem \
+  --embed-certs=true \
+  --server=https://127.0.0.1:6443 \
+  --kubeconfig=admin.kubeconfig
+
+kubectl config set-credentials admin \
+  --client-certificate=admin.pem \
+  --client-key=admin-key.pem \
+  --embed-certs=true \
+  --kubeconfig=admin.kubeconfig
+
+kubectl config set-context default \
+  --cluster=kubernetes-the-hard-way \
+  --user=admin \
+  --kubeconfig=admin.kubeconfig
+
+kubectl config use-context default --kubeconfig=admin.kubeconfig
