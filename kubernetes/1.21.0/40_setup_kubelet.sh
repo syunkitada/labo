0d5ae9b5e6ce5a4201b392bfbcd65b28cbbb9066
syunkitada <syun.kitada@gmail.com>
2021-06-27 21:57:56 +0900

    update

--- kubernetes/1.21.0/40_setup_kubelet.sh
+++ kubernetes/1.21.0/40_setup_kubelet.sh
@@ -25,7 +25,7 @@ fi
 if [ ! -e /usr/local/bin/runc ]; then
     wget "https://github.com/opencontainers/runc/releases/download/v1.0.0-rc93/runc.amd64"
     chmod +x runc.amd64
-    sudo mv runc.amd64 /usr/local/bin/
+    sudo mv runc.amd64 /usr/local/bin/runc
 fi
 
 if [ ! -e /opt/cni ]; then
