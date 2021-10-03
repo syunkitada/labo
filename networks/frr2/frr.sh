da7cd5cfc3f62c8c0d264c73a39ac170c1b0b81c
syunkitada <syun.kitada@gmail.com>
2021-10-03 23:43:39 +0900

    update

--- networks/frr2/frr.sh
+++ networks/frr2/frr.sh
@@ -4,7 +4,7 @@ COMMAND="${@:-help}"
 
 FRRVER="frr-stable"
 
-ROOT=`echo $PWD | awk -F setup-scripts '{print $1"setup-scripts/networks/frr1"}'`
+ROOT=`echo $PWD | awk -F setup-scripts '{print $1"setup-scripts/networks/frr2"}'`
 cd $ROOT
 
 function init-node() {
@@ -12,7 +12,7 @@ function init-node() {
     sudo docker ps | grep " ${name}$" && sudo docker kill ${name} || echo "${name} not found"
     sudo docker ps --all | grep " ${name}$" && sudo docker rm ${name} || echo "${name} not found"
     sudo docker run --name ${name} -d \
-        -it --privileged --rm --network none -v $ROOT/etc:/docker/etc --name $name centos7-frr-node:latest sh
+        -it --privileged --rm --network none -v /tmp/docker:/docker/tmp -v $ROOT/etc:/docker/etc --name $name centos7-frr-node:latest sh
 
     test -e /var/run/netns/$name && rm /var/run/netns/$name || echo "removed ns $name"
     PID=`sudo docker inspect --format '{{.State.Pid}}' $name`
@@ -33,9 +33,56 @@ function init-interface() {
 
 function init-frr() {
     name=$1
+    as=$2
+    ip=$3
+    interfaces=$4
+
     sudo docker exec $name ln -sf /docker/etc/frr/daemons /etc/frr/daemons
-    sudo docker exec $name ln -sf /docker/etc/frr/$name-frr.conf /etc/frr/frr.conf
-    sudo docker exec $name /usr/lib/frr/frrinit.sh start
+
+    interfacesConf=""
+    bgpNeighborsConf=""
+    for interface in $interfaces
+    do
+        interfacesConf+="
+interface ${interface}
+  ipv6 nd ra-interval 10
+  no ipv6 nd suppress-ra
+!
+"
+        bgpNeighborsConf+="neighbor ${interface} interface peer-group ADMIN
+  "
+    done
+
+    cat << EOS | sudo tee /tmp/docker/$name-frr.conf
+frr defaults datacenter
+hostname hv1
+
+log file /var/log/frr/frr.log
+!
+
+${interfacesConf}
+
+router bgp ${as}
+  bgp router-id ${ip}
+  bgp bestpath as-path multipath-relax
+
+  neighbor ADMIN peer-group
+  neighbor ADMIN remote-as external
+  neighbor ADMIN capability extended-nexthop
+
+  ${bgpNeighborsConf}
+
+  address-family ipv4 unicast
+    network ${ip}/32
+  exit-address-family
+!
+
+line vty
+!
+EOS
+
+    sudo docker exec $name ln -sf /docker/tmp/$name-frr.conf /etc/frr/frr.conf
+    sudo docker exec $name /usr/lib/frr/frrinit.sh restart
     sudo docker exec $name /usr/lib/frr/frrinit.sh reload
 }
 
@@ -75,6 +122,10 @@ function init() {
 
     link-node sp11 lf111
     link-node sp11 lf112
+    link-node sp11 lf121
+    link-node sp11 lf122
+    link-node sp12 lf111
+    link-node sp12 lf112
     link-node sp12 lf121
     link-node sp12 lf122
 
@@ -91,9 +142,20 @@ function init() {
     init-interface sp12 10.1.30.12
 
     # init frr
-    # init-frr hv1
-    # init-frr lf1
-    # init-frr sp1
+    init-frrs
+}
+
+function init-frrs() {
+    init-frr hv111 4200110111 10.1.10.111 "lf111-hv111-2 lf112-hv111-2"
+    init-frr hv112 4200110112 10.1.10.112 "lf111-hv112-2 lf112-hv112-2"
+    init-frr hv121 4200110121 10.1.10.121 "lf121-hv121-2 lf122-hv121-2"
+    init-frr hv122 4200110122 10.1.10.122 "lf121-hv122-2 lf122-hv122-2"
+    init-frr lf111 4200120111 10.1.20.11  "lf111-hv111-1 lf111-hv112-1 sp11-lf111-2 sp12-lf111-2"
+    init-frr lf112 4200120112 10.1.20.12  "lf112-hv111-1 lf112-hv112-1 sp11-lf112-2 sp12-lf112-2"
+    init-frr lf121 4200120111 10.1.20.21  "lf121-hv121-1 lf121-hv122-1 sp11-lf121-2 sp12-lf121-2"
+    init-frr lf122 4200120112 10.1.20.22  "lf122-hv121-1 lf122-hv122-1 sp11-lf122-2 sp12-lf122-2"
+    init-frr sp11  4200130011 10.1.30.11  "sp11-lf111-1 sp11-lf112-1 sp11-lf121-1 sp11-lf122-1"
+    init-frr sp12  4200130012 10.1.30.12  "sp12-lf111-1 sp12-lf112-1 sp12-lf121-1 sp12-lf122-1"
 }
 
 
