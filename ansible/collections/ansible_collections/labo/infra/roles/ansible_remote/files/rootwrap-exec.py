912b0299c6d9245aa8a3b88a567475a29164cec5
syunkitada <syun.kitada@gmail.com>
2024-04-15 01:25:47 +0900

    update

---
+++ ansible/collections/ansible_collections/labo/infra/roles/ansible_remote/files/rootwrap-exec.py
@@ -0,0 +1,27 @@
+#!/opt/ansible-nonroot/bin/python3
+
+import sys
+from oslo_rootwrap import client
+from multiprocessing import managers
+
+def main():
+    cmd = sys.argv[1:]
+
+    with open("/etc/ansible/rootwrap-socket-info", "rb") as f:
+        socket_path = f.readline()[:-1].decode('utf-8')
+        authkey = f.read(32)
+
+    manager = client.ClientManager(socket_path, authkey)
+    manager.connect()
+    proxy = manager.rootwrap()
+    stdin = None
+    return_code, stdout, stderr = proxy.run_one_command(cmd, None)
+
+    if stdout:
+        print(stdout)
+    if stderr:
+        print(stderr, file=sys.stderr)
+    exit(return_code)
+
+if __name__ == '__main__':
+    main()
